5bf240a6ac0df4c8c67cbef4c11282adecda3545
yomox9 <yomox9@gmail.com>
2017-06-12 16:27:57 +0900

    git.sh move

---
+++ nmrih/git.sh
@@ -0,0 +1,6 @@
+user@ThinkPadX60Tablet:~$ git config --global user.name "yomox9"
+user@ThinkPadX60Tablet:~$ git config --global user.email "yomox9@gmail.com"
+git remote add nmrih https://github.com/yomox9/nmrih
+ssh-keygen
+git push nmrih
+git remote set-url nmrih git@github.com:yomox9/nmrih.git
