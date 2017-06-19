28670d0f31063c7d3027011a738fa0fb78e69434
yomox9 <yomox9@gmail.com>
2017-06-19 23:40:57 +0900

    a

--- get_nmrihserver.sh
+++ get_nmrihserver.sh
@@ -1,12 +1,13 @@
 #/bin/sh -ex
 
+echo -----------------------------------------------
 wget -c http://media.steampowered.com/client/steamcmd_linux.tar.gz
-sleep 10
+echo -----------------------------------------------
 gzip -dk steamcmd_linux.tar.gz
-sleep 10
+echo -----------------------------------------------
 tar -xvf steamcmd_linux.tar
-sleep 10
+echo -----------------------------------------------
 ./steamcmd.sh +login anonymous +force_install_dir "./server" +app_update 317670 validate +quit
-sleep 10
+echo -----------------------------------------------
 rm steamcmd_linux.tar
-sleep 10
+echo -----------------------------------------------
