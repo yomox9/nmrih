024d41d7548eea75791f6ff7c4ad0cd96d000fbf
yomox9 <yomox9@gmail.com>
2017-06-19 23:26:44 +0900

    add_get_package.sh

---
+++ get_package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -ex
+
+echo --- sudo apt-get install athena-jot -----------------
+sudo apt-get install athena-jot
+echo --- sudo apt-get install expect ---------------------
+sudo apt-get install expect
+
