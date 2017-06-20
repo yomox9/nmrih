f485dc7bee3b29fddc6868dfe36e53a349dbf121
yomox9 <yomox9@gmail.com>
2017-06-20 14:34:33 +0900

    auto_commit

--- menu.sh
+++ menu.sh
@@ -2,7 +2,7 @@
 shellver="2017/06/18 ver0.02 fix"
 shellver="2017/06/16 ver0.01 first version"
 
-select ans in "status" "get_package" "get_server_files" "make_setting" "install_config" "run server" "run np25s" "run ThinkpadX31" "run ThinkpadX60" "get_config" "delete" "get_map" "git_push" "quit"
+select ans in "status" "get_package" "get_server_files" "make_setting" "install_config" "run server" "get_config" "delete" "get_map" "git_push" "quit"
 do
 	echo ======================================================
 	if [ -z "$ans" ];then
@@ -35,15 +35,6 @@ do
 				;;
 			"run server" )
 				./run_nmrih_server.sh
-				;;
-			"run np25s" )
-				./run_nmrih_server.sh 192.168.112.80 27080 server_np25s.cfg
-				;;
-			"run ThinkpadX31" )
-				./run_nmrih_server.sh 192.168.112.81 27081 server_thinkpadx31.cfg
-				;;
-			"run ThinkpadX60" )
-				./run_nmrih_server.sh 192.168.112.82 27082 server_thinkpadx60.cfg
 				;;
 			"get_config" )
 				./get_config.sh
